269bb111c9372c74c41797ff5f2580a86616b12e
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2019-02-09 12:49:28 +0100

    Add files via upload

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,8 @@
+using Test
+using DelimitedFiles
+push!(LOAD_PATH,"C:\\Users\\Joy\\.julia\\dev")
+using SpectralEnvelope
 
+cd(@__DIR__)
+test = readdlm("DNA_data.txt")
+@test spectral_envelope(test)[2][5] == 0.17470002350548378
