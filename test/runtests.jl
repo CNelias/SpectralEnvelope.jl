45d622324ecd216967006b4ea911e458efe25d00
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2019-02-12 15:14:51 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,4 @@ using SpectralEnvelope
 
 cd(@__DIR__)
 test = readdlm("DNA_data.txt")
-@test spectral_envelope(test)[2][5] == 0.17470002350548297
+@test signif(spectral_envelope(test)[2][5],4) == signif(0.17470002350548297,4)
