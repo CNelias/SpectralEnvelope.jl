f7954322ad373cca6ae2243d6c0c80c4dfa8e027
Corentin Nélias <34754896+johncwok@users.noreply.github.com>
2019-05-28 09:34:29 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,4 +7,4 @@ cd(@__DIR__)
 test = readdlm("DNA_data.txt")
 x,y,e = spectral_envelope(test;m=0)
 @test round(spectral_envelope(test)[2][5]; digits = 5) == round(0.17470002350548297;digits = 5)
-@test get_mappings(0.33,x,y,e,["a","c","g","t"])[1] == "a : -0.3959646304004004"
+@test get_mappings(0.33,x,y,e,["a","c","g","t"])[1] == "a : -0.396"
