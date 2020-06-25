d1634fe828cddcc86bdaa443d1a247955a6bc8fa
Corentin Nelias <cwokpoubelle@laposte.net>
2020-06-25 11:55:33 +0200

    testing tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,4 +7,5 @@ cd(@__DIR__)
 test = readdlm("DNA_data.txt")
 x,y,e,c = spectral_envelope(test;m=0)
 @test round(spectral_envelope(test)[2][5]; digits = 5) == round(0.17517853879301779;digits = 5)
+print(get_mappings(0.33,x,y,e,c))
 @test get_mappings(0.33,x,y,e,c)[1] == "A : 0.702"
