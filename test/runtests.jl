09820191f6e1c8928a4f04747c28c7aba131b88b
Corentin Nélias <corentin.nelias@gmail.com>
2020-08-28 12:36:28 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,4 @@ using Random
 cd(@__DIR__)
 test = readdlm("DNA_data.txt")
 x,y,e = spectral_envelope(test; m=0)
-@test round(spectral_envelope(test)[2][5]; digits = 3) == round(0.129285; digits = 3)
+@test round(spectral_envelope(test)[2][5]; digits = 3) == round(0.126; digits = 3)
