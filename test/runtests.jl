69b213539526cac0aa5da24c981a5068ddc6167f
Corentin Nélias <corentin.nelias@gmail.com>
2021-10-08 12:12:24 +0200

    Adding test for ```get_mappings``` function

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,4 @@ cd(@__DIR__)
 test = readdlm("DNA_data.txt",',')[1,:]
 x,y,e = spectral_envelope(test; m=0)
 @test round(spectral_envelope(test)[2][5]; digits = 3) == round(0.003; digits = 3)
+@test round(get_mappings(test, 0.33)["A"]; digits = 2) == round(0.54; digits = 2)
